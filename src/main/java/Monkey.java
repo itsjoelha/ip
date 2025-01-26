a9d2b6f3a9cb69a89fe81f0b979c40fd67e8d1c5
Joel <joel.ha@gmail.com>
2025-01-26 22:26:54 +0800

    Added tasks & list

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,7 +1,27 @@
 import java.util.Scanner;
 
 public class Monkey {
+    String[] tasks = new String[100];
+    int taskCounter = 0;
+
+    public void toAdd(String input) {
+        tasks[taskCounter] = input;
+        taskCounter++;
+        System.out.println("added: " + input);
+    }
+
+    public void list() {
+        if (taskCounter == 0) {
+            System.out.println("No tasks yet.");
+        } else {
+            for (int i = 0; i < taskCounter; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        Monkey monkey = new Monkey();
         String input;
         Scanner in = new Scanner(System.in);
 
@@ -12,13 +32,16 @@ public class Monkey {
 
         while (true) {
             input = in.nextLine();
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("--------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("--------------------------------");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                monkey.list();
+            } else {
+                monkey.toAdd(input);
             }
-            System.out.println(input);
         }
     }
 }
