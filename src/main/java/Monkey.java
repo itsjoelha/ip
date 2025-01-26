6bb2402551813a98435cff37eebd8b6b5d20f60c
Joel <joel.ha@gmail.com>
2025-01-26 21:36:55 +0800

    Level-1 Echo

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
+
 public class Monkey {
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        */
+        String input;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("--------------------------------");
         System.out.println("Hello! I'm Monkey!");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("--------------------------------");
+
+        while (true) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("--------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("--------------------------------");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
