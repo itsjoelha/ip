e1e591c7a6cb97b0e9e2e8f40cc01e47b9f4d184
Joel <joel.ha@gmail.com>
2025-02-04 22:33:44 +0800

    Revert "Update event status & icon"
    This reverts commit df33e5e723e34961f45389d928bbd0a9421464c7.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,12 +2,12 @@ public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String by) {
-        super(description, "deadline");
+        super(description, "[D]");
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + "[" + getStatusIcon() + "] " + description + " (by: " + by + ")";
+        return super.toString() + " (by: " + by + ")";
     }
 }
