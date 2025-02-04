9952d26ae42fcc09edb46613e8efca41698985c4
Joel <joel.ha@gmail.com>
2025-02-04 22:34:36 +0800

    Update event status & icon

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,13 +3,13 @@ public class Event extends Task {
     protected String to;
 
     public Event(String description, String from, String to) {
-        super(description, "[E]");
+        super(description, "event");
         this.from = from;
         this.to = to;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + "[" + getStatusIcon() + "] " + description + " (from: " + from + " to: " + to + ")";
     }
 }
