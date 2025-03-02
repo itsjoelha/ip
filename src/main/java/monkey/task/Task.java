cd427a9db683c3cffc4cec8e47b3ad3b01f4612b
Joel <joel.ha@gmail.com>
2025-03-02 20:18:19 +0800

    Added JavaDoc comments for Task.java

--- src/main/java/monkey/task/Task.java
+++ src/main/java/monkey/task/Task.java
@@ -1,10 +1,25 @@
     package monkey.task;
 
+    /**
+     * The Task class represents a task with a description, completion status, and
+     * a task type.
+     */
     public abstract class Task {
+        /** The description of the task. */
         protected String description;
+
+        /** Indicates whether the task is completed. */
         protected boolean isDone;
+
+        /** The type of task. */
         protected String taskType;
 
+        /**
+         * Creates a Task with the given description and task type.
+         * By default, the task is not completed.
+         * @param description The description of the task.
+         * @param taskType The type of task.
+         */
         public Task(String description, String taskType) {
             this.description = description;
             this.isDone = false;
